6171eee598b9e0b422de04ad115c24723f65dcc3
DebbieLevy <128624905+DebbieLevy@users.noreply.github.com>
2023-03-22 11:55:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('BREAKFAST FAVORITES')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text("🥗Kale,Spinach & Rocket Smoothie")
