0aa5f25e5a4c06112fa7260bc1cea7f9310f9df3
DebbieLevy <128624905+DebbieLevy@users.noreply.github.com>
2023-03-22 11:46:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('BREAKFAST MENU')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text("Kale,Spinach & Rocket Smoothie")
