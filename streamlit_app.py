8c13f0d20310af6f398ddb2508de8de2cbfd6c12
DebbieLevy <128624905+DebbieLevy@users.noreply.github.com>
2023-03-22 11:54:48 -0500

    Update streamlit_app.py
    Added a new menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 streamlit.header('BREAKFAST FAVORITES')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text("🥗Kale,Spinach & Rocket Smoothie")
 streamlit.text('🥚Hard Boiled free range Egg')
 streamlit.text('🥑Avocado Toast')
+streamlit.text('🍌🍎 Create your own fruit Smoothie 🍑🍒')
